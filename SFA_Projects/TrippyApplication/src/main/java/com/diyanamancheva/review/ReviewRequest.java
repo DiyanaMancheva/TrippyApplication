52d0eacc062c7a1290f66e6e7bca1ee6de0225ba
DiyanaMancheva <diyanastoilova@gmail.com>
2023-04-28 12:10:54 +0300

    Create ReviewRequest

---
+++ SFA_Projects/TrippyApplication/src/main/java/com/diyanamancheva/review/ReviewRequest.java
@@ -0,0 +1,47 @@
+package com.diyanamancheva.review;
+
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Positive;
+import java.time.LocalDate;
+
+public class ReviewRequest {
+  @Positive(message = "UserId must be greater than 0")
+  private int userId;
+  @Positive(message = "VenueId must be greater than 0")
+  private int venueId;
+  private LocalDate creationDate;
+  @Pattern(regexp = "[0-5](1)", message = "Rating must not be between 0 and 5")
+  private float rating;
+  private String text;
+
+  public ReviewRequest(){}
+
+  public ReviewRequest(int userId, int venueId, LocalDate creationDate,
+                       float rating, String text){
+    this.userId = userId;
+    this.venueId = venueId;
+    this.creationDate = creationDate;
+    this.rating = rating;
+    this.text = text;
+  }
+
+  public int getUserId() {
+    return userId;
+  }
+
+  public int getVenueId() {
+    return venueId;
+  }
+
+  public LocalDate getCreationDate() {
+    return creationDate;
+  }
+
+  public float getRating() {
+    return rating;
+  }
+
+  public String getText() {
+    return text;
+  }
+}
