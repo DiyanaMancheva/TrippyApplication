eb102c7791bfed3496be82d8ee703601c618675f
DiyanaMancheva <diyanastoilova@gmail.com>
2023-04-28 11:51:00 +0300

    Add addReview() to ReviewService

--- SFA_Projects/TrippyApplication/src/main/java/com/diyanamancheva/review/ReviewService.java
+++ SFA_Projects/TrippyApplication/src/main/java/com/diyanamancheva/review/ReviewService.java
@@ -1,19 +1,30 @@
 package com.diyanamancheva.review;
 
+import com.diyanamancheva.user.User;
+import com.diyanamancheva.user.UserService;
+import com.diyanamancheva.venue.Venue;
+import com.diyanamancheva.venue.VenueService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
 public class ReviewService {
   private ReviewAccessor reviewAccessor;
   private ReviewMapper reviewMapper;
+  private UserService userService;
+  private VenueService venueService;
+
 
   @Autowired
-  public ReviewService(ReviewAccessor reviewAccessor, ReviewMapper reviewMapper){
+  public ReviewService(ReviewAccessor reviewAccessor, ReviewMapper reviewMapper,
+                       UserService userService, VenueService venueService){
     this.reviewAccessor = reviewAccessor;
     this.reviewMapper = reviewMapper;
+    this.userService = userService;
+    this.venueService = venueService;
   }
 
   public List<ReviewDto> getAllReviews(){
@@ -27,5 +38,14 @@ public class ReviewService {
     Review review = reviewAccessor.readReviewById(id);
 
     return review;
+  }
+
+  public void addReview(int userId, int venueId, LocalDate creationDate,
+                        float rating, String text) {
+    User user = userService.getUserById(userId);
+    Venue venue = venueService.getVenueById(venueId);
+
+    Review review = new Review(user, venue, creationDate, rating, text);
+    reviewAccessor.addReview(review);
   }
 }
