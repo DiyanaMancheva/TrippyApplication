3acea7299d6d21ff3fa7fa34dcc09f202f2b7d3c
DiyanaMancheva <diyanastoilova@gmail.com>
2023-04-28 14:11:08 +0300

    Add addUser() to UserService

--- SFA_Projects/TrippyApplication/src/main/java/com/diyanamancheva/user/UserService.java
+++ SFA_Projects/TrippyApplication/src/main/java/com/diyanamancheva/user/UserService.java
@@ -1,19 +1,27 @@
 package com.diyanamancheva.user;
 
+import com.diyanamancheva.city.City;
+import com.diyanamancheva.city.CityService;
+import com.diyanamancheva.review.Review;
+import com.diyanamancheva.venue.Venue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
 public class UserService {
   private UserAccessor userAccessor;
   private UserMapper userMapper;
+  private CityService cityService;
 
   @Autowired
-  public UserService(UserAccessor userAccessor, UserMapper userMapper){
+  public UserService(UserAccessor userAccessor, UserMapper userMapper,
+                     CityService cityService){
     this.userAccessor = userAccessor;
     this.userMapper = userMapper;
+    this.cityService = cityService;
   }
 
   public List<UserDto> getAllUsers(){
@@ -27,5 +35,15 @@ public class UserService {
     User user = userAccessor.readUserById(id);
 
     return  user;
+  }
+
+  public User addUser(String username, int cityId,
+                      String email, LocalDate joinDate) {
+    City city = cityService.getCityById(cityId);
+
+    User user = new User(username, city, email, joinDate);
+    userAccessor.addUser(user);
+
+    return user;
   }
 }
